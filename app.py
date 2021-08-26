9c6aefe1a0afe8e5d7d5ca7e4d9557ab662bc6b5
arkhoms13 <51392485+arkhoms13@users.noreply.github.com>
2021-08-27 06:15:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -33,6 +33,9 @@ def index():
     """Video streaming home page."""
     return render_template('index.html')
 
+@app.route('/test')
+def test():
+    return 'test'
 
 if __name__ == '__main__':
     app.run(debug=True)
